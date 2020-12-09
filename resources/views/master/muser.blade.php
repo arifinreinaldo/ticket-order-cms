d5f350db81808f9cbcb485b19968dcb290983a5e
Reinaldo <reinaldo.arifin.slamet@mhealth.tech>
2020-12-09 22:40:12 +0700

    bug fix error message

--- resources/views/master/muser.blade.php
+++ resources/views/master/muser.blade.php
@@ -63,21 +63,6 @@
                 <h5>User Admin Panel</h5>
             </div>
             <div class="card-block table-border-style">
-                @if(session('success'))
-                    <div class="alert alert-success" role="alert">
-                        {{session('success')}}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                @elseif(session('failed'))
-                    <div class="alert alert-danger" role="alert">
-                        {{session('failed')}}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                @endif
                 <div class="table-responsive">
                     <table class="table table-striped" id="dataTable">
                         <thead>
